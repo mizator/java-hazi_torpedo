7d711900087c5bafca2c7c84750162b44dcf4607
Áron RADICS <raronkonektor@gmail.com>
2017-05-20 02:39:21 +0200

    javadoc

--- src/asd/Message.java
+++ src/asd/Message.java
@@ -3,11 +3,30 @@ package asd;
 import java.awt.Point;
 import java.io.Serializable;
 
+/**
+ * A ket fel kozti halozati kapcsolaton atkuldendo uzenetet tartalmazo osztaly
+ *
+ */
 public class Message implements Serializable {
+	/**
+	 * Az uzenetnek ilyen tipusai lehetnek 
+	 */
 	public enum Tipus {Loves, NemTalalt, Talat, TalaltSullyedt};
+	/**
+	 * Az uzenet tipusa
+	 */
 	Tipus tipus;
+	
+	/**
+	 * Melyik cellara vonatkozik az uzenet
+	 */
 	public Point loc;
 	
+	/**
+	 * Letrehoz egy Message objektumot
+	 * @param t tipussal
+	 * @param l cella pozicioval
+	 */
 	public Message(Tipus t, Point l)
 	{
 		tipus = t;
